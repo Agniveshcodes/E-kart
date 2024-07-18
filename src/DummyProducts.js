6d66a4eae64d90853a06e8a3c90d0d2192c281ed
Agnivesh <singhagnivesh678@gmail.com>
2024-07-18 17:00:16 +0530

    cart bug fixed

--- src/DummyProducts.js
+++ src/DummyProducts.js
@@ -16,7 +16,7 @@ const products = [
     image:
       "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
     category: "T-shirt",
-      rating: 4.1,
+    rating: 4.1,
   },
 ];
 
